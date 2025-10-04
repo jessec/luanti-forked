ae1a6153870ef0d9186956c318fc36bcd86359c1
trimm <robertjessecooke@gmail.com>
2025-10-04 12:18:04 +0700

    cc

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -78,7 +78,7 @@ void set_default_settings()
 	settings->setDefault("language", "");
 	settings->setDefault("name", "");
 	settings->setDefault("bind_address", "");
-	settings->setDefault("serverlist_url", "https://eduquest.vip/serverlist.json");
+	settings->setDefault("serverlist_url", "https://eduquest.vip");
 
 	// Client
 	settings->setDefault("address", "");
